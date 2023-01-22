5ef1e92e3106f4d13b2244a86c2ea5fbaf16ed6b
Nazaruk-D <nazaruk-dima@mail.ru>
2023-01-22 14:57:08 +0300

    Task title update

--- src/app/todos/components/todos/todo/tasks/task/task.component.ts
+++ src/app/todos/components/todos/todo/tasks/task/task.component.ts
@@ -13,21 +13,40 @@ export class TaskComponent {
   @Output() changeTaskEvent = new EventEmitter<{todoId: string, taskId: string; model: UpdateTaskModel }>()
 
   taskStatusEnum = TaskStatusEnum
+  newTitle = ''
+  editMode = false
 
   removeTaskHandler() {
     this.removeTaskEvent.emit({todoId: this.task.todoListId, taskId: this.task.id})
   }
 
+
+  activateEditModeHandler() {
+    this.editMode = true
+    this.newTitle = this.task.title
+  }
+
+  editTitleHandler() {
+    this.changeTask({title: this.newTitle})
+    this.newTitle = ''
+    this.editMode = false
+  }
+
   changeTaskStatusHandler(event: MouseEvent) {
     const newStatus = (event.currentTarget as HTMLInputElement).checked
+    this.changeTask({  status: newStatus ? TaskStatusEnum.completed: TaskStatusEnum.active})
+  }
+
+  changeTask(patch: Partial<UpdateTaskModel>) {
     const model: UpdateTaskModel = {
-      status: newStatus ? TaskStatusEnum.completed: TaskStatusEnum.active,
-      title: this.task.title,
       completed: this.task.completed,
       deadline: this.task.deadline,
       description: this.task.description,
       priority: this.task.priority,
-      startDate: this.task.startDate
+      startDate: this.task.startDate,
+      status: this.task.status,
+      title: this.task.title,
+      ...patch
     }
     this.changeTaskEvent.emit({todoId: this.task.todoListId, taskId: this.task.id, model} )
   }
