96cba1dea0b991d657185ff32fabcda0d72d3079
Nazaruk-D <nazaruk-dima@mail.ru>
2023-01-23 20:34:35 +0300

    Form typing

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -9,15 +9,15 @@ import {AuthService} from "../../../core/services/auth.service";
 })
 export class LoginComponent {
   loginForm = new FormGroup({
-    email: new FormControl('', [
-      Validators.required,
-      Validators.pattern('[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,5}$'),
-    ]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3)
-    ]),
-    rememberMe: new FormControl(false)
+    email: new FormControl<string>('', {nonNullable: true, validators: [
+        Validators.required,
+        Validators.pattern('[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,5}$'),
+      ]}),
+    password: new FormControl<string>('', {nonNullable: true, validators: [
+        Validators.required,
+        Validators.minLength(3)
+      ]}),
+    rememberMe: new FormControl<boolean>(false, {nonNullable: true})
   })
 
   constructor(private authService: AuthService) {
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -5,12 +5,19 @@ import {CommonResponse} from "../models/core.models";
 import {ResultCodeEnum} from "../enums/resultCode.enum";
 import {Router} from "@angular/router";
 
+export interface LoginRequestData {
+  email: string
+  password: string
+  rememberMe: boolean
+}
+
+
 @Injectable()
 export class AuthService {
 
   constructor(private http: HttpClient, private router: Router) { }
 
-  login(data: any) {
+  login(data: Partial<LoginRequestData>) {
     this.http.post<CommonResponse<{userId: number}>>(`${environment.baseURL}/auth/login`, data)
       .subscribe((res) => {
         if(res.resultCode === ResultCodeEnum.success) {
